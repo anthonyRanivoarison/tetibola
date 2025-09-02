6308c48076200b48f7890eea8662096a2f10bc7b
Anerti <e.andrianombana@protonmail.com>
2025-09-02 18:00:12 +0300

    file structure

---
+++ apps/server/routes/auth.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+const authRouter = Router();
+
+//authRouter.post('/signup', userCreation);
+
+//authRouter.post('/signup/verification', verifyEmail);
+
+//authRouter.post('/login', userAuth);
+
+//authRouter.post('/login/verification', verifyEmailOnLogin);
+
+// authRouter.get('/me', getUserProfile); // user profile
+
+export default authRouter;
