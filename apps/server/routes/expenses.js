1b71af882b9c97d492b686067cf37a0c52d69b7e
shaim <hei.shaim@gmail.com>
2025-09-09 19:58:46 +0200

    fix : the bug in controleur ,route and querie

--- apps/server/routes/expenses.js
+++ apps/server/routes/expenses.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import multer from "multer";
 import { getExpenses, getExpensesById, postExpenses, putExpensesById, deleteExpensesById } from "../controllers/expenseController.js";
+import {verifyAuthToken} from "../controllers/authControllers.js";
 
 const upload = multer({ dest: "uploads/" });
 const expensesRouter = Router();
 
-expensesRouter.get("/", getExpenses);
-expensesRouter.get("/:id", getExpensesById);
-expensesRouter.post("/", upload.single("receipt_upload"), postExpenses);
-expensesRouter.put("/:id", upload.single("receipt_upload"), putExpensesById);
-expensesRouter.delete("/:id", deleteExpensesById);
+expensesRouter.get("/", verifyAuthToken, getExpenses);
+expensesRouter.get("/:id", verifyAuthToken, getExpensesById);
+expensesRouter.post("/", verifyAuthToken, upload.single("receipt_upload"), postExpenses);
+expensesRouter.put("/:id", verifyAuthToken, upload.single("receipt_upload"), putExpensesById);
+expensesRouter.delete("/:id", verifyAuthToken, deleteExpensesById);
 
 export default expensesRouter;
