d214890cc033478d1a05207808755710f9205bd7
Hiraina <hei.hiraina@gmail.com>
2025-09-10 23:23:27 +0300

    feat(profile): add POST route to create user profile

--- apps/server/routes/profile.js
+++ apps/server/routes/profile.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { getProfile, updateProfile } from "../controllers/profileController.js";
+import { getProfile, updateProfile, createProfile } from "../controllers/profileController.js";
 import { verifyAuthToken } from "../controllers/authControllers.js";
 
 const profileRouter = express.Router();
 
 
 router.get("/", verifyAuthToken, getProfile);
+router.post("/", verifyAuthToken, createProfile);
 router.put("/", verifyAuthToken, updateProfile);
 
 export default profileRouter;
